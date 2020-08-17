351c086f54444e7bc0e511555911ec2655bd0724
Vishal Negi <tropbon9@gmail.com>
2020-08-17 08:37:49 +0530

    backup changes config

---
+++ .config/i3/scripts/wallpaper.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# while true
+# do
+#     feh --randomize --bg-fill ~/.config/i3/wallpaper/*
+#     sleep 10
+# done
+
--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -9,5 +9,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch Polybar, using default config location ~/.config/polybar/config
 #polybar mybar &
 polybar example >>/tmp/polybar1.log 2>&1 &
+#polybar example1 -c ~/.config/polybar/config_top &
+#polybar example2 -c ~/.config/polybar/config_bottom
 
 echo "Polybar launched..."
